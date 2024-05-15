1f1f4c06a106a0b02a0c3204d4124373b2bae0b2
lucadekyndt <luca.dekyndt@epitech.eu>
2024-05-15 13:28:36 +0200

    fix: display memory without printf

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -7,22 +7,35 @@
 
 #include "../../include/src.h"
 
+void uint8_to_hex(u_int8_t value, char *hex_buffer)
+{
+    const char hex_digits[] = "0123456789ABCDEF";
+
+    hex_buffer[0] = hex_digits[(value >> 4) & 0xF];
+    hex_buffer[1] = hex_digits[value & 0xF];
+}
+
 static void display_return_line(int i)
 {
     if (i % 32 == 0) {
         if (i != 0) {
-            printf("\n");
+            my_putchar("\n");
         }
+        return;
     }
+    my_putchar(' ');
 }
 
 void display_memory(u_int8_t *memoire, int taille)
 {
+    char hex_buffer[3];
+
     for (int i = 0; i < taille; i++) {
         display_return_line(i);
-        printf("%02X ", memoire[i]);
+        uint8_to_hex(memoire[i], hex_buffer);
+        my_putstr(hex_buffer);
     }
-    printf("\n");
+    my_putchar("\n");
 }
 
 u_int8_t *init_memory(void)
