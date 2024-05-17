bbda95af6985b44a3e095f42e33a7da61ddbddb2
charliinew <charles.ignoux@epitech.eu>
2024-05-17 16:22:42 +0200

    feat: functionnal instruction fork

--- src/instr/my_fork.c
+++ src/instr/my_fork.c
@@ -9,5 +9,11 @@
 
 void my_fork(corewar_t *corewar, champion_t *champion)
 {
+    int arg = reconstruct_int(corewar->memory,
+        (champion->PC + 1) % MEM_SIZE, 2);
+    int child_pc = (champion->PC + arg % IDX_MOD) % MEM_SIZE;
+
+    champion->cycle_instruction = 799;
+    add_child(corewar, champion, child_pc, 799);
     return;
 }
