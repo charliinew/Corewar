63c356a87e44f438aebc75a015ba9096e9f71fea
charliinew <charles.ignoux@epitech.eu>
2024-05-17 16:29:20 +0200

    feat: functionnal instruction lfork

--- src/instr/my_lfork.c
+++ src/instr/my_lfork.c
@@ -9,5 +9,11 @@
 
 void my_lfork(corewar_t *corewar, champion_t *champion)
 {
+    int arg = reconstruct_int(corewar->memory,
+        (champion->PC + 1) % MEM_SIZE, 2);
+    int child_pc = (champion->PC + arg) % MEM_SIZE;
+
+    champion->cycle_instruction = 999;
+    add_child(corewar, champion, child_pc, 999);
     return;
 }
