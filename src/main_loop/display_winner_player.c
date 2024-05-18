1731e6caf9da84143f42eded84e85ffdfc997ed2
lucadekyndt <luca.dekyndt@epitech.eu>
2024-05-18 04:10:09 +0200

    fix: fix display winner

--- src/main_loop/display_winner_player.c
+++ src/main_loop/display_winner_player.c
@@ -13,9 +13,9 @@ void display_winner_player(corewar_t *corewar)
     champion_t *tmp = *corewar->champion;
 
     for (; tmp != NULL; tmp = tmp->next) {
-        if (tmp->live != -1) {
+        if (tmp->last_live == 1) {
             my_putstr("The player ");
-            my_put_nbr(tmp->id);
+            my_put_nbr(tmp->reg[0]);
             my_putstr("(");
             my_putstr(tmp->header->prog_name);
             my_putstr(") has won.\n");
